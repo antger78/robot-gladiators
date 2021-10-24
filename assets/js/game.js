aff31e987d2211408af99870262c2a8f037d48e0
Anthony Geritano <anthony.geritano@aya.yale.edu>
2021-10-24 18:49:59 -0400

    doing more with the enemy

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,24 @@
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log (playerName + " is ready for battle!");
+console.log (playerName, playerAttack, playerHealth);
 
-var name = "your name";
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(name);
-
-function fight () {
-    window.alert("The fight has begun!");
-}
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+  
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  
+    // Log a resulting message to the console so we know that it worked.
+  
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  
+    // Log a resulting message to the console so we know that it worked.
+  };
 
 fight();
