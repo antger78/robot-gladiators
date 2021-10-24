17aead7f16c8fc1bba328b160d7c02f7052ab1b0
Anthony Geritano <anthony.geritano@aya.yale.edu>
2021-10-24 17:24:15 -0400

    added some variables and stuff

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+console.log (playerName + " is ready for battle!");
+
+var name = "your name";
+
+console.log(name);
+
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+fight();
